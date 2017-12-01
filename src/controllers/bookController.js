2e582ee916b3fd02d5e5eaf960fcc750a72a0401
TatianaMontoya <tatianamontoya15@gmail.com>
2017-12-01 16:25:56 -0500

    Servicios

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -11,14 +11,47 @@ exports.listAllBooks = function (req, res) {
     });
 };
 
+exports.findByTitleOrAuthor = function (req, res) {
+    let obj = req.body;
+    Books.find({ $or: [{ 'title': { $regex: "^.*" + obj.title + ".*$" } }, { 'author': { $regex: "^.*" + obj.author + ".*$" } }] }, function (err, books) {
+        if (err)
+            res.status(400).send(err);
+        res.status(200).json(books);
+    });
+};
 
 exports.createBook = function (req, res) {
-    console.log(req.body);
     let newBook = new Books(req.body);
     newBook.save(function (err, task) {
         if (err) {
             res.status(400).send(err);
         }
         res.status(201).json(task);
+    });
+};
+
+exports.getBook = function (req, res) {
+    Books.findById(req.params.idBook, function (err, book) {
+        if (err)
+            res.status(400).send(err);
+        res.status(200).json(book);
+    });
+};
+
+exports.updateBook = function (req, res) {
+    Books.findOneAndUpdate({ _id: req.params.idBook }, req.body, { new: true }, function (err, book) {
+        if (err)
+            res.status(400).send(err);
+        res.status(201).json(book);
+    });
+};
+
+exports.deleteBook = function (req, res) {
+    Books.remove({
+        _id: req.params.idBook
+    }, function (err, task) {
+        if (err)
+            res.status(400).send(err);
+        res.status(201).json({ result: true });
     });
 };
--- src/routes/booksRoutes.js
+++ src/routes/booksRoutes.js
@@ -4,8 +4,14 @@ module.exports = function (app) {
     var bookController = require('../controllers/bookController');
 
     // Books Routes
-    app.route('/book/create')
-        .post(bookController.createBook);
-
+    app.route('/book/create').post(bookController.createBook);
+    //Obtiene todos los libros
     app.route('/books').get(bookController.listAllBooks);
+    //Obtiene la información de un libro
+    app.route('/book/:idBook')
+        .get(bookController.getBook)
+        .put(bookController.updateBook)
+        .delete(bookController.deleteBook);
+    //Busca libros por titulo o autor
+    app.route('/books/findByTitleOrAuthor').post(bookController.findByTitleOrAuthor);
 };
